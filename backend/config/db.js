04b08b6edaaa5075c9f2b60542348d0bf93c0dbb
Hrisabh Yadav <hrisabhyadav31@gmail.com>
2024-02-16 15:39:29 +0530

    DB connection setup done

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const env = require("./envConfig");
+const connect = async () => {
+  try {
+    await mongoose.connect(env.URL);
+    console.log("Db connection established");
+  } catch (error) {
+    console.log(error.message);
+    process.exit;
+  }
+};
+
+module.exports = connect;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const app = express();
 
 const env = require("./config/envConfig");
+const connect = require("./config/db");
+
+const app = express();
+
+// Database connection
+connect();
 
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to revibe" });
