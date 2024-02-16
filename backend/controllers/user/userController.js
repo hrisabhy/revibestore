fd87933c044e710053a6d5a91ede7c9d2a04cc6e
Hrisabh Yadav <hrisabhyadav31@gmail.com>
2024-02-16 17:37:08 +0530

    Register controller function done

--- backend/controllers/user/userController.js
+++ backend/controllers/user/userController.js
@@ -1,10 +1,31 @@
 const { validationResult } = require("express-validator");
 const UserModel = require("../../models/User");
+const { hashPassword } = require("../../services/authService");
 
-module.exports.register = (req, res) => {
+module.exports.register = async (req, res) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
-    console.log(errors.array());
+    const { name, email, password } = req.body;
+    try {
+      const emailExists = await UserModel.findOne({ email: email });
+      if (!emailExists) {
+        const hashed = await hashPassword(password);
+        const user = await UserModel.create({
+          name,
+          email,
+          password: hashed,
+          admin: true,
+        });
+        return res
+          .status(200)
+          .json({ message: "Your account has been registered" });
+      } else {
+        return res.status(401).json({ message: "Email already used" });
+      }
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).json("Internal Server Error");
+    }
   } else {
     return res.status(400).json({ errors: errors.array() });
   }
--- backend/routes/user/userRoutes.js
+++ backend/routes/user/userRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { registerValidations } = require("../../validations/userValidations");
-const { validationResult } = require("express-validator");
 const { register } = require("../../controllers/user/userController");
 const router = express.Router();
 router.post("/register", registerValidations, register);
