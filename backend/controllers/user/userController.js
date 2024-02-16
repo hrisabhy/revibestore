0b23be90633b67144e3b552fcf2389c5dfbe1e7a
Hrisabh Yadav <hrisabhyadav31@gmail.com>
2024-02-16 16:50:45 +0530

    User model implemented

--- backend/controllers/user/userController.js
+++ backend/controllers/user/userController.js
@@ -1,10 +1,11 @@
 const { validationResult } = require("express-validator");
+const UserModel = require("../../models/User");
 
 module.exports.register = (req, res) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
+  if (errors.isEmpty()) {
     console.log(errors.array());
   } else {
-    console.log(req.body);
+    return res.status(400).json({ errors: errors.array() });
   }
 };
