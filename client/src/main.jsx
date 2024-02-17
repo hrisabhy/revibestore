e0d99bb63e745a484a991b053680c38cc2f56efb
hrisabhy <hrisabhyadav31@gmail.com>
2024-02-17 22:09:31 +0530

    Auth reducer setup done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import Store from "./store/index.jsx";
 import "./input.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
