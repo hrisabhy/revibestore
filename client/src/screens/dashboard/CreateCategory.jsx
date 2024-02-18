7eeac0fc26940923256c57cd561d6f441e9cebdc
hrisabhy <hrisabhyadav31@gmail.com>
2024-02-18 14:56:22 +0530

    create categgory api call added

--- client/src/screens/dashboard/CreateCategory.jsx
+++ client/src/screens/dashboard/CreateCategory.jsx
@@ -1,7 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ScreenHeader from "../../components/ScreenHeader";
 import Wrapper from "./Wrapper";
+import { useEffect, useState } from "react";
+import { useCreateMutation } from "../../store/services/categoryService";
 const CreateCategory = () => {
+  const [state, setState] = useState("");
+  const [saveCategory, data] = useCreateMutation();
+  console.log(data);
+  const errors = data?.error?.data?.errors ? data?.error?.data?.errors : [];
+  const submitCategory = (e) => {
+    e.preventDefault();
+    saveCategory({ name: state });
+  };
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (data?.isSuccess) {
+      navigate("/dashboard/categories");
+    }
+  }, [data?.isSuccess]);
   return (
     <Wrapper>
       <ScreenHeader>
@@ -9,18 +25,30 @@ const CreateCategory = () => {
           <i className="bi bi-arrow-left-short"></i> categories list
         </Link>
       </ScreenHeader>
-      <form className="w-full md:w-8/12">
+      <form className="w-full md:w-8/12" onSubmit={submitCategory}>
         <h3 className="text-lg capitalize mb-3">create category</h3>
+        {errors.length > 0 &&
+          errors.map((error, key) => (
+            <p className="alert-danger" key={key}>
+              {error.msg}
+            </p>
+          ))}
         <div className="mb-3">
           <input
             type="text"
             name=""
             className="form-control"
             placeholder="Category Name..."
+            value={state}
+            onChange={(e) => setState(e.target.value)}
           />
         </div>
         <div className="mb-3">
-          <input type="submit" value="create category" className="btn-indigo" />
+          <input
+            type="submit"
+            value={data.isLoading ? "loading..." : "create category"}
+            className="btn-indigo"
+          />
         </div>
       </form>
     </Wrapper>
--- client/src/store/index.jsx
+++ client/src/store/index.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authService from "./services/authService";
 import authReducer from "./reducers/authReducer";
+import categoryService from "./services/categoryService";
 const Store = configureStore({
   reducer: {
     [authService.reducerPath]: authService.reducer,
+    [categoryService.reducerPath]: categoryService.reducer,
     authReducer: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
