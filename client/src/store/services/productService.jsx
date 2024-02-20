14d697646440e788f2b4bc2fecdcf75b6c7c8d82
hrisabhy <hrisabhyadav31@gmail.com>
2024-02-20 20:21:00 +0530

    Show products with pagination

--- client/src/store/services/productService.jsx
+++ client/src/store/services/productService.jsx
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const productService = createApi({
   reducerPath: "products",
+  tagTypes: "products",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:5000/api/",
     prepareHeaders: (headers, { getState }) => {
@@ -22,6 +23,7 @@ const productService = createApi({
             body: data,
           };
         },
+        invalidatesTags: ["products"],
       }),
       getProducts: builder.query({
         query: (page) => {
@@ -30,6 +32,7 @@ const productService = createApi({
             method: "GET",
           };
         },
+        providesTags: ["products"],
       }),
     };
   },
