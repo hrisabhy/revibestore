e023c5831cb6165a30a5ee5d9541b18c5574fe70
hrisabhy <hrisabhyadav31@gmail.com>
2024-02-21 18:17:13 +0530

    Skeleton added

--- client/src/components/home/Categories.jsx
+++ client/src/components/home/Categories.jsx
@@ -2,50 +2,84 @@ import { Virtual } from "swiper/modules";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useAllCategoriesQuery } from "../../store/services/categoryService";
-
+import Skeleton from "../skeleton/Skeleton";
+import Thumbnail from "../skeleton/Thumbnail";
 const Categories = () => {
   const { data, isFetching } = useAllCategoriesQuery();
-  console.log(data);
-  return isFetching
-    ? "Loading..."
-    : data?.categories.length > 0 && (
-        <Swiper
-          modules={[Virtual]}
-          spaceBetween={20}
-          slidesPerView={3}
-          virtual
-          className="w-full h-[150px] mb-10"
-          breakpoints={{
-            0: { slidesPerView: 2 },
-            640: { slidesPerView: 3 },
-            768: { slidesPerView: 4 },
-            1080: { slidesPerView: 5 },
-            1280: { slidesPerView: 6 },
-          }}
+  let i = 1;
+  return isFetching ? (
+    <div className="flex flex-wrap -mx-4 mb-10">
+      {[1, 2, 3, 4, 5, 6].map((item) => (
+        <div
+          className="w-6/12 sm:w-4/12 md:w-3/12 lg:w-[20%] xl:w-2/12 p-4"
+          key={item}
         >
-          {data.categories.map((category, index) => (
+          <Skeleton>
+            <Thumbnail />
+          </Skeleton>
+        </div>
+      ))}
+    </div>
+  ) : (
+    data?.categories.length > 0 && (
+      <Swiper
+        modules={[Virtual]}
+        spaceBetween={20}
+        slidesPerView={3}
+        virtual
+        className="w-full h-[150px] mb-10"
+        breakpoints={{
+          0: {
+            slidesPerView: 2,
+          },
+          640: {
+            slidesPerView: 3,
+          },
+          768: {
+            slidesPerView: 4,
+          },
+          1080: {
+            slidesPerView: 5,
+          },
+          1280: {
+            slidesPerView: 6,
+          },
+        }}
+      >
+        {data.categories.map((category, index) => {
+          if (i >= 5) {
+            i = 1;
+          } else {
+            i++;
+          }
+          return (
             <SwiperSlide
-              key={category._id || index} // Use category ID as key if available
+              className="w-full overflow-hidden  rounded-lg relative"
+              key={index}
               virtualIndex={index}
-              className="w-full overflow-hidden rounded-lg relative"
             >
-              <img
-                src={`./images/slider/${category.imageId || index + 1}.jpg`}
-                alt={category.name}
-                className="w-full h-full object-cover"
-              />
+              <div className="w-full h-[150px] rounded-lg overflow-hidden">
+                <img
+                  src={`./images/slider/${i}.jpg`}
+                  className="w-full h-full object-cover"
+                  alt=""
+                />
+              </div>
               <div className="absolute inset-0 w-full h-full bg-black/50 flex items-center justify-center p-4">
                 <Link
-                  to={`/category/${category._id || index + 1}`} // Use category ID in URL if available
+                  to="/"
                   className="text-white text-base font-medium capitalize"
                 >
                   {category.name}
                 </Link>
               </div>
             </SwiperSlide>
-          ))}
-        </Swiper>
-      );
+          );
+        })}
+      </Swiper>
+    )
+  );
+  return <div>Categories</div>;
 };
 
 export default Categories;
